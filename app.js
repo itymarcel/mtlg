8f9d0fe96553ffd4ab98a7c9b1f0000ea0527382
Marcel <marcel@udiocollective.com>
2020-11-19 15:30:09 +0000

    1

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>MTLG server running whooop</h1>');
 });
 
 server.listen(port,() => {
